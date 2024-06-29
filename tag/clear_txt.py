d0e37939209cc217a4ee6d8b4b7c8829ca068f99
windsing <117965575+wochenlong@users.noreply.github.com>
2024-06-29 20:02:03 +0800

    删除没有同名图片文件的txt文件

---
+++ tag/clear_txt.py
@@ -0,0 +1,17 @@
+import os
+
+def delete_txt_without_image(folder_path):
+    # 遍历指定文件夹中的所有文件
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".txt"):
+            txt_file = os.path.join(folder_path, filename)
+            image_file = os.path.join(folder_path, os.path.splitext(filename)[0])
+            # 检查是否存在同名的图片文件
+            if not any([os.path.isfile(image_file + ext) for ext in [".webp", ".jpg", ".png"]]):
+                # 删除没有同名图片文件的txt文件
+                os.remove(txt_file)
+                print(f"Deleted {txt_file}")
+
+# 指定文件夹路径
+folder_path = r"F:\data\pose\shoulder_carry\1_p"
+delete_txt_without_image(folder_path)
