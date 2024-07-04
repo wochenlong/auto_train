6c74830afb1733327ee8a499fa16416c5a2f29a0
windsing <117965575+wochenlong@users.noreply.github.com>
2024-07-05 03:07:16 +0800

    更新了jpeg文件

--- tag/clear_txt.py
+++ tag/clear_txt.py
@@ -7,11 +7,11 @@ def delete_txt_without_image(folder_path):
             txt_file = os.path.join(folder_path, filename)
             image_file = os.path.join(folder_path, os.path.splitext(filename)[0])
             # 检查是否存在同名的图片文件
-            if not any([os.path.isfile(image_file + ext) for ext in [".webp", ".jpg", ".png"]]):
+            if not any([os.path.isfile(image_file + ext) for ext in [".webp", ".jpeg",".jpg", ".png"]]):
                 # 删除没有同名图片文件的txt文件
                 os.remove(txt_file)
                 print(f"Deleted {txt_file}")
 
 # 指定文件夹路径
-folder_path = r"F:\data\pose\shoulder_carry\1_p"
+folder_path = r"C:\Users\PC\Downloads\魔道\train\wangji_new"
 delete_txt_without_image(folder_path)
